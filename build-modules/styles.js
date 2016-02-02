aa5ad2c94ae905129d1cf6f2d1ba19e894e2d37d
jh3y <jh3y@users.noreply.github.com>
2016-02-02 19:48:14 +0000

    Remove theme files from dist and correct license commenting in stylus files

--- build-modules/styles.js
+++ build-modules/styles.js
@@ -15,12 +15,10 @@ var gulp      = require('gulp'),
   },
   /* styles:compile */
   compile = function() {
-    var licenseComment = '/*!\n  * driveway\n  *\n  * @author jh3y\n  * @license MIT\n*/\n';
     return gulp.src(src.styles)
       .pipe(plugins.plumber())
       .pipe(plugins.stylus())
       .pipe(plugins.prefix(opts.prefix))
-      .pipe(env.dist ? plugins.header(licenseComment): plugins.gUtil.noop())
       .pipe(env.stat ? plugins.size(opts.gSize): plugins.gUtil.noop())
       .pipe(env.deploy ? plugins.gUtil.noop(): gulp.dest(env.dist ? dest.dist: dest.css))
       .pipe(plugins.minify())
